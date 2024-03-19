dcef91da5999151defc3d14df61e572c087c89ee
Isaiah Okon <izaya4real@gmail.com>
2024-03-19 09:08:49 +0100

    Update on sentence case

--- src/components/InputOutput.jsx
+++ src/components/InputOutput.jsx
@@ -3,36 +3,44 @@ import './InputOutput.css';
 import TextLength from "./TextLength";
 
 
+
 export const Input = (props) => {
 
  const [todos, setTodos] = useState([]);
 
  const [inputValue, setInputValue] = useState('');
 
+
  const inputChange = (e) => {
   setInputValue(e.target.value);
  };
 
+
  const addTodo = () => {
-  if (inputValue !== "" && inputValue.length > 5 && inputValue.length < 21) {
-   setTodos([...todos, inputValue.trim()]);
+  const input = sentenceCase(inputValue);
+  if (input !== "" && input.length > 5 && input.length < 21) {
+   setTodos([...todos, input]);
    setInputValue('');
   } else {
    return alert("Can not add your todo, because text length is either too short or too long.");
   }
  };
 
+
  const editTodo = (index) => {
   const editTodoAlert = confirm("Do you want to edit this todo?");
   if (editTodoAlert) {
-   const newTodo = prompt("Edit todo:");
-   if (newTodo !== "" && newTodo !== null) {
+   const newTodo = sentenceCase(prompt("Edit todo:"));
+   if (newTodo !== "" && newTodo !== null && newTodo !== "" && newTodo.length > 5 && newTodo.length < 21) {
     const updatedTodo = [...todos];
-    updatedTodo[index] = newTodo.trim();
+    updatedTodo[index] = newTodo;
     setTodos(updatedTodo);
+   } else {
+    return alert("Can not save your todo, because text length is either too short or too long.");
    }
   }
  }
+
 
  const deleteTodo = (index) => {
   const deleteAlert = confirm("Are you sure?");
@@ -42,6 +50,14 @@ export const Input = (props) => {
    setTodos(newTodos);
   }
  };
+
+
+ const sentenceCase = (word) => {
+  const input = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  const output = input.trim()
+  return output;
+ };
+
 
  return (
   <>
